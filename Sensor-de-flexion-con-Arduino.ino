4de5476339901e34c6a64ade2844c235073d5067
TEOS <proyectoteos@gmail.com>
2018-06-13 23:11:13 -0500

    Code and diagram available
    Program code and assembly diagram

---
+++ Sensor-de-flexion-con-Arduino.ino
@@ -0,0 +1,22 @@
+const int flexPinT = A0;
+const float resistanceT = 47000.0;
+const float minimumBendingT = 23000.0;
+const float maximumBendingT = 36000.0;
+int angleDegreesT;
+float flexionT;
+String resultSerialT;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(flexPinT, INPUT);
+}
+
+void loop() {
+  flexionT = resistanceT * (5.0 / (analogRead(flexPinT) * 5.0 / 1023.0) - 1.0);
+  angleDegreesT = map(flexionT, minimumBendingT, maximumBendingT, 0, 90);
+  angleDegreesT = constrain(angleDegreesT, 0, 90);
+  resultSerialT = "Sensor a: " + String(angleDegreesT) + " grados";
+  Serial.println(resultSerialT);
+  delay(300);
+}
+
